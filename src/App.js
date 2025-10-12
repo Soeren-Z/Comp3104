64424292b50c5949df4a1bac25df1b31c6bbb0b9
Harrison Pape <harrison.pape@georgebrown.ca>
2025-10-12 01:33:39 -0400

    name change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101520368 || Harrison Pape || DevOps</h5>
+      <h5>Developed By: 101520368 || Harrison Pape || COMP 3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
